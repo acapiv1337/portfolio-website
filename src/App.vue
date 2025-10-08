dd28fe0abffb920cb7a5d8a796d640ad5e56f717
acapan <amirlasyrf.00@gmail.com>
2025-10-08 23:24:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app" @click="unmuteMusic">
+    <!-- Full-screen GIF -->
+    <div class="gif-fullscreen"></div>
+
+    <!-- Hidden audio element -->
+    <audio ref="bgMusic" autoplay loop muted>
+      <source src="/bon_voyage.mp3" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YoohyeonFullScreen",
+  methods: {
+    unmuteMusic() {
+      const audio = this.$refs.bgMusic;
+      if (audio.muted) {
+        audio.muted = false;
+        audio.play();
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.gif-fullscreen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('/yoohyeon.gif'); /* Replace with your GIF */
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  z-index: 0;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer; /* indicate user can click */
+}
+</style>
